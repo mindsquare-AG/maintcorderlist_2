48d331b9de9dfb607e6dbe01c2d37930c42e8d7e
Alban Elezi <aelezi@ikons-see.com>
2023-08-28 09:38:24 +0000

    Alban change filter activity

--- webapp/controller/OrderDetails.controller.js
+++ webapp/controller/OrderDetails.controller.js
@@ -259,6 +259,24 @@ sap.ui.define([
 
 		onArbplChange: function (oEvent) {
 			sap.ui.getCore().byId("selectArbpl").setValueState("None");
+
+			//Alban Change 8/21/2023 call backend service to filter leistungsart
+			var oObjId = oEvent.getParameters().selectedItem.getAdditionalText();
+			var aFilter = [];
+			var sPath = "/ActivityTypeSet";
+			aFilter.push(new Filter('ObjId', FilterOperator.EQ, oObjId));
+
+			this.getView().getModel("generalServices").read(sPath, {
+				filters: [aFilter],
+				success: function (oData) {
+					var oModel = new JSONModel(oData.results);
+					this.getView().setModel(oModel, "learrItemNew");
+				}.bind(this),
+				error: function (oError) {
+
+				}.bind(this)
+			});
+			//Alban Change 8/21/2023 call backend service to filter leistungsart
 		},
 
 		onLearrChange: function (oEvent) {
@@ -284,7 +302,7 @@ sap.ui.define([
 			} catch (e) {
 
 			}
-			
+
 			oBookTime.IdaurFull = oBookTime.IdaurFull.replace(',', '.');
 
 			// If not deleted this will cause an backend error - therefore we first delete the __metadata property
